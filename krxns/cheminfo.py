6dc69420c71446f72b74782d94915a23d2e9b401
stefanpate <stefanpate94@gmail.com>
2024-09-16 17:50:13 -0500

    Added draw reaction and molecule fcns to cheminfo. Started new known reaction net filtering process in notebook

--- krxns/cheminfo.py
+++ krxns/cheminfo.py
@@ -1,14 +1,58 @@
 import rdkit
 from rdkit import Chem
-from rdkit.Chem import rdFMCS, rdFingerprintGenerator, BRICS
+from rdkit.Chem import rdFMCS, rdFingerprintGenerator, BRICS, Draw, Mol
 from rdkit.Chem.MolStandardize import rdMolStandardize
+from rdkit.Chem.rdChemReactions import ChemicalReaction
 from itertools import product, chain
 import re
 import pandas
 import numpy as np
 from typing import Iterable
 
-def tautomer_expand(molecule: rdkit.Chem.rdchem.Mol | str, k: int) -> list[rdkit.Chem.rdchem.Mol] | list[str]:
+def draw_molecule(mol: str | ChemicalReaction, size: tuple = (200, 200), use_svg: bool = True):
+    '''
+    Draw molecule.
+
+    Args
+    ----
+    mol:str | rdkit.Chem.ChemicalReaction
+        Molecule
+    size:tuple
+        (width, height)
+    use_svg:bool
+    '''
+    if type(mol) is str:
+        mol = Chem.MolFromSmiles(mol)
+
+    if use_svg:
+        drawer = Draw.MolDraw2DSVG(*size)
+        drawer.DrawMolecule(mol)
+        drawer.FinishDrawing()
+        img = drawer.GetDrawingText()
+    else:
+        img = Draw.MolToImage(mol, size=size)
+
+    return img
+
+def draw_reaction(rxn: str | Mol, sub_img_size: tuple = (200, 200), use_svg: bool = True, use_smiles: bool = True):
+    '''
+    Draw reaction.
+
+    Args
+    ----
+    rxn:str
+        SMARTS-encoded reaction
+    sub_img_size:tuple
+        Substrate img size
+    use_svg:bool
+    use_smiles:bool
+    '''
+    if type(rxn) is str:
+        rxn = Chem.rdChemReactions.ReactionFromSmarts(rxn, useSmiles=use_smiles)
+
+    return Draw.ReactionToImage(rxn, useSVG=use_svg, subImgSize=sub_img_size)
+
+def tautomer_expand(molecule: Mol | str, k: int) -> list[Mol] | list[str]:
     '''
     Returns top k tautomers of given molecule in the
     format provided.
