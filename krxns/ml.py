Fixed scaling issues with val data, including changing featurize data helper in ml.py. Started basic nb for loss curve monitoring. Started predict.py. Changed experiment saving protocol

krxns/ml.py
@@ -2,9 +2,9 @@
 Machine learning helpers
 '''
 import pandas as pd
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 from sklearn.model_selection import StratifiedKFold
-from typing import Any
+from chemprop.featurizers.base import GraphFeaturizer
 from chemprop import data
 from pathlib import Path
 
@@ -24,18 +24,9 @@ def load_data(data_dir: Path, n_chunks: int = None) -> pd.DataFrame:
     
     return df
 
-# TODO: Get the right types for featurizer and scaler
-def featurize_data(df: pd.DataFrame, featurizer:Any, train_mode:bool) -> tuple[DataLoader, Any]:
+def featurize_data(df: pd.DataFrame, featurizer: GraphFeaturizer) -> Dataset:
     '''
-    Converts dataframe with SMILES data to dataloader.
-
-    Args
-    ----
-    df
-    featurizer
-    train_mode:bool
-        If true will shuffle batches and return a target scaler, else does not shuffle
-        and returned scaler is None
+    Converts dataframe with SMILES data to torch dataset.
     '''
     smiles_cols = ['starter_smiles', 'target_smiles']
     target_cols = ['spl']
@@ -45,18 +36,9 @@ def featurize_data(df: pd.DataFrame, featurizer:Any, train_mode:bool) -> tuple[D
     datapoints += [[data.MoleculeDatapoint.from_smi(smis[i]) for smis in smiss] for i in range(1, len(smiles_cols))]
     datasets = [data.MoleculeDataset(datapoints[i], featurizer) for i in range(len(smiles_cols))]
     mc_dataset = data.MulticomponentDataset(datasets=datasets)
+    return mc_dataset
 
-    if train_mode:
-        scaler = mc_dataset.normalize_targets()
-    else:
-        scaler = None
-
-    dataloader = data.build_dataloader(mc_dataset, shuffle=train_mode)
-    
-    return dataloader, scaler
-
-def split_data(df: pd.DataFrame, split_idx: int) -> tuple[pd.DataFrame]:
-    k = 5
+def split_data(df: pd.DataFrame, split_idx: int, k: int = 5) -> tuple[pd.DataFrame]:
     if split_idx < 0 or split_idx > k:
         raise ValueError(f"Provided split index {split_idx} not between 0 and {k}")
     skf = StratifiedKFold(n_splits=k, shuffle=False)

scripts/train.py
@@ -20,6 +20,7 @@ from lightning.pytorch.loggers import CSVLogger
 from chemprop import featurizers, nn
 from chemprop.nn import metrics
 from chemprop.models import multi
+from chemprop.data import build_dataloader
 from argparse import ArgumentParser
 from krxns.config import filepaths
 from krxns.ml import load_data, split_data, featurize_data
@@ -29,18 +30,35 @@ parser.add_argument("data_dir", help="Path dir containing chunks, relative to co
 parser.add_argument("n_chunks", type=int, help="Number of data chunks to load")
 parser.add_argument("max_epochs", type=int, help="Max number training epochs")
 parser.add_argument("--split-idx", type=int, default=-1, help="CV split form 0 to k - 1. If not provided, train on all data")
-parser.add_argument("--experiment-name", default=None, help="Organize logs into subdir")
+parser.add_argument("--experiment", default=None, help="Put all conditions from this experiment in a subdir")
+parser.add_argument("--condition", default=None, help="Put all data splits for a condition in a subsbudir")
+save_dir = filepaths['spl_cv'] # TODO: Make this CL? How get in shell script? Read hydra docs and sh scrip tutorial
 
 def main(args):
+    cross_val = args.split_idx >= 0
     all_data = load_data(filepaths['data'] / args.data_dir, args.n_chunks)
     featurizer = featurizers.SimpleMoleculeMolGraphFeaturizer() # TODO: extend to others w/ CL arg selecting
 
-    if args.split_idx >= 0:
+    if cross_val:
+        # Split
         train_data, val_data = split_data(all_data, args.split_idx)
-        train_data, scaler = featurize_data(train_data, featurizer, train_mode=True) # Should only shuffle for train dataloader (see chemprop docs)
-        val_data, _ = featurize_data(val_data, featurizer, train_mode=False)
+
+        # Featurize
+        train_data = featurize_data(train_data, featurizer) 
+        val_data = featurize_data(val_data, featurizer)
+
+        # Scale
+        scaler = train_data.normalize_targets()
+        val_data.normalize_targets(scaler)
+
+        # Loader
+        train_dataloader = build_dataloader(train_data, shuffle=True) # Should only shuffle for train dataloader (see chemprop docs)
+        val_dataloader = build_dataloader(val_data, shuffle=False)
     else:
-        train_data, scaler = featurize_data(all_data, featurizer, train_mode=True)
+        train_data = featurize_data(all_data, featurizer) # Featurize
+        scaler = train_data.normalize_targets() # Scale
+        train_dataloader = build_dataloader(train_data) # Loader
+        val_dataloader = None # Lightning Trainer default
 
     # Construct model
     mcmp = nn.MulticomponentMessagePassing(blocks=[nn.BondMessagePassing()], n_components=2, shared=True)
@@ -51,8 +69,8 @@ def main(args):
     mcmpnn = multi.MulticomponentMPNN(mcmp, agg, ffn, metrics=metric_list, batch_norm=True)
 
     # Fit
-    save_dir = filepaths['spl_cv'] / f"hp_0" / f"split_{args.split_idx}"
-    logger = CSVLogger(save_dir=save_dir, name=args.experiment_name, version=f"version_0") # TODO: will probably change
+    version = f"{args.condition}/split_{args.split_idx}"
+    logger = CSVLogger(save_dir=save_dir, name=args.experiment, version=version)
     trainer = Trainer(
         logger=logger,
         enable_progress_bar=True,
@@ -61,7 +79,7 @@ def main(args):
         max_epochs=args.max_epochs, # number of epochs to train for
     )
     tic = perf_counter()
-    trainer.fit(model=mcmpnn, train_dataloaders=train_data, val_dataloaders=val_data)
+    trainer.fit(model=mcmpnn, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
     toc = perf_counter()
     print(f"Training took: {toc - tic:.2f} seconds")
 