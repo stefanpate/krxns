e11f1b0bd2dae031992f1a3d9b305f1ac3646b39
stefanpate <stefanpate94@gmail.com>
2025-05-29 15:52:31 -0500

    Simplified and updated construct_reaction_network and modified connect reactions script to orient around unique cpd ids instead of rct and pdt indices

--- krxns/net_construction.py
+++ krxns/net_construction.py
@@ -1,412 +1,65 @@
-from rdkit import Chem
-from rdkit.Chem import AllChem, Mol
-from krxns.cheminfo import post_standardize
-from itertools import permutations, chain, product, combinations
-from collections import defaultdict, Counter
 from typing import Iterable
-import numpy as np
-from scipy.stats import entropy
 import pandas as pd
-from tqdm import tqdm
 
 def construct_reaction_network(
-        operator_connections: dict[int, dict],
-        reactions: dict[int, dict],
-        atom_lb: float = 0.0,
-        coreactant_whitelist: Iterable = None,
-        add_multi_mol_nodes: bool = False
-
+        mass_inlinks: dict[str, dict[str, dict[str, float]]],
+        compounds: pd.DataFrame,
+        sources: Iterable[str] = [],
     ):
     '''
     Args
-    -------
-
+    ----
+    mass_inlinks:dict
+        {reaction_id: {product_id: {reactant_id: fraction_atoms_from_reactant}, ...}, ...}
+        Fraction of atoms in a product coming from a reactant.
+    compounds:pd.DataFrame
+        DataFrame containing compound information with 'id', 'smiles' and 'name' columns.
+    
     Returns
-    ---------
+    -------
     edges:list[tuple]
         Entries are (from:int, to:int, properties:dict)
     nodes:list[tuple]
         Entries are (id:int, properties:dict)
     '''
-    include_edge_props = ('smarts', 'rhea_ids', 'imt_rules')
-    direction_from_side = lambda side : 0 if side == 'rct_inlinks' else 1 if side == 'pdt_inlinks' else print("Error side key not found")
-
-    reactions = fold_reactions(reactions)
-    compounds, smi2id = extract_compounds(reactions)
-    id2smi = {v: k for k, v in smi2id.items()}
-    unpaired_whitelist = [k for k in coreactant_whitelist if coreactant_whitelist[k] is None]
-    
-    tmp_edges = []
-
-    # Add from operator_connections
-    for rid, sides in operator_connections.items():
-        smiles = [elt.split('.') for elt in reactions[rid]['smarts'].split('>>')]
-
-        for side, inlinks in sides.items():
-            direction = direction_from_side(side) # This is a binary switch to tranpose the adjacency matrix
-            inlinks = remove_unpaired_whitelist(inlinks, direction, smiles, unpaired_whitelist) # Remove unpaired coreactants
-            adj_mat = translate_operator_adj_mat(inlinks, direction, smiles, smi2id) # Convert nestes inlinks dict to adjacency matrix
-            edge_props = { **{'rid': rid}, **{prop: reactions[rid][prop] for prop in include_edge_props} }
-
-            if direction == 0:
-                edge_props['smarts'] = ">>".join(edge_props['smarts'].split(">>")[::-1])
-
-            tmp_edges += nested_adj_mat_to_edge_list(adj_mat, edge_props, smi2id, id2smi, atom_lb, coreactant_whitelist, add_multi_mol_nodes)
-
     edges = []
-    next_node_id = 0
-    mols_to_node_id = {}
-    nodes = []
-    for elt in tmp_edges:
-        new_elt = []
-        mols_st = elt[:2] # Starter target molecules tuples
-        for mols in mols_st:
-            if mols in mols_to_node_id:
-                this_node_id = mols_to_node_id[mols]
-            else:
-                mols_to_node_id[mols] = next_node_id
-                next_node_id += 1
-                this_node_id = mols_to_node_id[mols]
-                smiles, names = zip(*[(compounds[m]['smiles'], compounds[m]['name']) for m in mols])
-                smiles = ".".join(smiles)
-                nodes.append((this_node_id, {'smiles': smiles, 'names': names, 'cpd_ids': mols}))
-            
-            new_elt.append(this_node_id)
+    nodes = {}
+    ep = 5e-3
+    for rid, pdt_inlinks in mass_inlinks.items():
+        for pdt_id, rcts in pdt_inlinks.items():
+            this_sources = set(u for u in rcts if u in sources)
+            for rct_id, mass_frac in rcts.items():
+                source_mass = sum(rcts[s] for s in this_sources - {rct_id})
 
-        new_elt.append(elt[-1])
-        new_elt = tuple(new_elt)
-        edges.append(new_elt)
+                if (mass_frac + source_mass) >= 1.0 - ep:
+                    edges.append(
+                        (
+                            rct_id,
+                            pdt_id,
+                            {
+                                'reaction_id': rid,
+                                'mass_frac': mass_frac,
+                                'coreactants': this_sources,
+                                'coproducts': set(pdt_inlinks.keys()) - {pdt_id},
+                            }
+                        )
+                    )
+
+                    nodes[rct_id] = (rct_id, compounds.loc[compounds.id == rct_id, ['smiles', 'name']].to_dict())
+                    nodes[pdt_id] = (pdt_id, compounds.loc[compounds.id == pdt_id, ['smiles', 'name']].to_dict())
 
     return edges, nodes
-        
-def remove_unpaired_whitelist(adj_mat: dict[int, dict[int, float]], direction: int, smiles: list[str], unpaired_whitelist: Iterable[str]):
-    tmp = defaultdict(lambda : defaultdict(float))
-    for i, inner in adj_mat.items():
-        ismi = smiles[direction ^ 0][i]
-                
-        if ismi in unpaired_whitelist:
-            continue
-    
-        for j, atom_frac in inner.items():
-            jsmi = smiles[direction ^ 1][j]
-            
-            if jsmi in unpaired_whitelist:
-                continue
-
-            tmp[i][j] = atom_frac
-
-    return tmp
-
-# def handle_multiple_rules(rules: dict[str, dict]):
-#     first_rule = next(iter(rules))
-
-#     if len(rules) == 1:
-#         return rules[first_rule]
-    
-#     exactly_same = True
-#     directionally_same = True
-
-#     rct_inlinks_check = defaultdict(set)
-#     pdt_inlinks_check = defaultdict(set)
-#     for sides in rules.values():
-#         for side, outer in sides.items():
-#             for i, inner in outer.items():
-#                 for j, atom_frac in inner.items():
-#                     if side == 'rct_inlinks':
-#                         rct_inlinks_check[(i, j)].add(atom_frac)
-#                     elif side == 'pdt_inlinks':
-#                         pdt_inlinks_check[(i, j)].add(atom_frac)
-
-#     for v in rct_inlinks_check.values():
-#         if len(v) > 1:
-#             exactly_same = False
-#             break
-
-#     if exactly_same:
-#         for v in pdt_inlinks_check.values():
-#             if len(v) > 1:
-#                 exactly_same = False
-#                 break
-
-#     if exactly_same:
-#         return rules[first_rule]
-#     else:
-#         rct_inlinks_check = defaultdict(set)
-#         pdt_inlinks_check = defaultdict(set)
-#         for sides in rules.values():
-#             for side, outer in sides.items():
-#                 for i, inner in outer.items():
-#                     distro = list(inner.values())
-#                     if side == 'rct_inlinks':
-#                         rct_inlinks_check[i].add(np.argmax(distro))
-#                     elif side == 'pdt_inlinks':
-#                         pdt_inlinks_check[i].add(np.argmax(distro))
-
-#         for v in rct_inlinks_check.values():
-#             if len(v) > 1:
-#                 directionally_same = False
-#                 break
-
-#         if directionally_same:
-#             for v in pdt_inlinks_check.values():
-#                 if len(v) > 1:
-#                     directionally_same = False
-#                     break
-
-#         if directionally_same:
-#             return rules[first_rule]
-        
-#         return {}
-
-def translate_operator_adj_mat(adj_mat: dict[int: dict[int, float]], direction: int, smiles: str, smi2id: dict[str, int]) -> list[dict[int: dict[int, float]]]:
-    '''
-    Convert operator adjacency matrices, which have molecules labeled by order
-    of appearance in the reaction, to those with molecules labeled by their unique
-    ID per lexicographical sort of their SMILES. If there are multiples of the same
-    unique molecule, multiple adjacency matrices are returned.
-    '''
-    i2id = {i: smi2id[smiles[direction ^ 0][i]] for i in adj_mat}
-    j2id = {j: smi2id[smiles[direction ^ 1][j]] for j in next(iter(adj_mat.values()))}
-
-
-    id2i = defaultdict(list)
-    id2j = defaultdict(list)
-
-    for i, id in i2id.items():
-        id2i[id].append(i)
-
-    for j, id in j2id.items():
-        id2j[id].append(j)
-
-    has_repeats = lambda x : any([len(v) > 1 for v in x.values()])
-    if not has_repeats(id2i) and not has_repeats(id2j):
-        return [{i2id[i]: {j2id[j]: atom_frac for j, atom_frac in inner.items()} for i, inner in adj_mat.items()}]
-    else:
-        expanded_adj_mat = []
-        for i_combo in product(*id2i.values()):
-            for j_combo in product(*id2j.values()):
-                am = {i2id[i]: {j2id[j]: adj_mat[i][j] for j in j_combo} for i in i_combo}
-                expanded_adj_mat.append(am)
-        
-        return expanded_adj_mat
-
-def nested_adj_mat_to_edge_list(
-        adj_mat: list[dict[int: dict[int, float]]],
-        edge_props:dict,
-        smi2id:dict,
-        id2smi:dict,
-        atom_lb: float,
-        coreactant_whitelist: Iterable[str],
-        add_multi_mol_nodes: bool
-    ) -> list[tuple]:
-    '''
-    Converts (mutliple) m_pdts x n_rcts adjacency matrices into list of edges between
-    the molecules in those reactions, roughly respecting conservation of mass.
-
-    Args
-    -----
-    adj_mat: list[dict[int: dict[int, float]]]
-    edge_props:dict
-    smi2id:dict
-    id2smi:dict
-    atom_lb:float
-    coreactant_whitelist:Iterable[str]
-    add_multi_mol_nodes:bool
-    '''
-    get_cos = lambda candidates, exclude : dict(Counter([elt for elt in candidates if smi2id[elt] not in exclude]))
-    rcts, pdts = [side.split(".") for side in edge_props["smarts"].split(">>")]
-
-    def fails_whitelist_check(whitelist: dict, coreactants: dict, coproducts: dict):
-        '''
-        Returns True if required co-reactants are not in the whitelist or if they are
-        in the whitelist but co-products do not feature their canonical pairs.
-        Applied as final check of edge before adding to edgelist.
-        TODO: Still necessary? For similarity connections?
-        '''
-        for k in coreactants.keys():
-            if k not in whitelist:
-                return True
-            elif whitelist[k] is None: # Unpaired coreactant
-                pass
-            elif all([cp not in coproducts for cp in whitelist[k]]):
-                return True
-        return False
-    
-    def skip_paired_whitelist(successor: int, predecessors: Iterable[int], whitelist: dict[str, str], id2smi: dict[int, str]):
-        '''
-        Returns True if the successor is a paired whitelisted coreactant and its pair is among the predecessors
-        '''
-        successor_smi = id2smi[successor]
-        predecessor_smis = [id2smi[p] for p in predecessors]
-        if successor_smi not in whitelist:
-            return False
-        elif whitelist[successor_smi] in predecessor_smis:
-            return True
-        else:
-            return False
-
-    iids = adj_mat[0].keys()
-    
-    # Extract inlinks from sufficient (mixtures of) molecules
-    inlinks = {(i,): {'from':tuple(), 'atom_frac':-1} for i in iids}
-    for elt in adj_mat:
-
-        for i, inner in elt.items():
-
-            # Skip over paired whitelist coreactants
-            if skip_paired_whitelist(successor=i, predecessors=elt.keys(),
-                    whitelist=coreactant_whitelist, id2smi=id2smi):
-                continue
-
-            if add_multi_mol_nodes:
-                neighbor_fracs = [(k, v) for k, v in inner.items() if v > atom_lb]
-                
-                if neighbor_fracs:
-                    neighbor, atom_frac = zip(*neighbor_fracs)
-                    atom_frac = sum(atom_frac)
-                else:
-                    neighbor = tuple()
-                    atom_frac = -1
-
-            else:
-                neighbor, atom_frac = sorted(inner.items(), key=lambda x : x[1], reverse=True)[0] # Max
-                neighbor = (neighbor,)
-                
-            # If multiples of same unique mol, i, across multiple
-            # adj mats, take the max
-            if atom_frac > atom_lb and atom_frac > inlinks[(i,)]['atom_frac']:
-                inlinks[(i,)]['from'] = tuple(sorted(neighbor))
-                inlinks[(i,)]['atom_frac'] = atom_frac
-
-    if add_multi_mol_nodes:
-        combo_lens = [2, 3] # Just two and 3 mol combos for now
-        non_currency_outputs = [elt for elt in inlinks.keys() if id2smi[elt[0]] not in coreactant_whitelist]
-        multi_inlinks = {}
-        for k in combo_lens:
-            for combo in combinations(non_currency_outputs, k):
-                rows = []
-                for c in combo:
-                    sufficient_candidates = inlinks[c]['from']
-                    sufficient_candidates = set(filter(lambda x : id2smi[x] not in coreactant_whitelist, sufficient_candidates))
-                    rows.append(sufficient_candidates)
-                    
-                sufficient = set.union(*rows)
-                
-                if len(sufficient) > 0:
-                    sufficient = tuple(sorted(sufficient)) # Keep cpd ids sorted
-                    multi_source = {'from': sufficient, 'atom_frac': 1.0} # TODO: Get actual weighted average of atom_frac, for now this is fine
-                    multi_inlinks[tuple(sorted(chain(*combo)))] = multi_source # Keep cpd ids sorted
-
-        inlinks = {**inlinks, **multi_inlinks}
-
-    # Add edges to network edge list
-    triple_set = set()
-    edges = []
-    for i in inlinks:
-        if len(inlinks[i]['from']) == 0 or inlinks[i]['atom_frac'] == -1:
-            continue
-        
-        j = inlinks[i]['from']
-        coreactants = get_cos(candidates=rcts, exclude=j)
-        coproducts = get_cos(candidates=pdts, exclude=i)
-        atom_frac = inlinks[i]['atom_frac']
-
-        if coreactant_whitelist and fails_whitelist_check(coreactant_whitelist, coreactants, coproducts):
-            continue
-
-        # Ensure no repeats
-        # Note: gives (from, to, props) in order networkx expects
-        if (j, i, atom_frac) not in triple_set:
-            triple_set.add((j, i, atom_frac))
-            props = { **edge_props, **{'atom_frac':atom_frac, "coreactants": coreactants, "coproducts": coproducts} }
-            edges.append((j, i, props))
-
-    return edges
-
-def fold_reactions(reactions: dict[str, dict]):
-    reactions = {int(k): v for k,v in reactions.items()} # Cast keys to int
-
-    # Remove reverses
-    rids = set()
-    for k, v in reactions.items():
-        rids.add(tuple(sorted([k, v['reverse']])))
-
-    keepers = [elt[0] for elt in rids]
-    reactions = {k: reactions[k] for k in keepers}
-
-    return reactions
-
-def extract_compounds(reactions: dict):
-        '''
-        Extracts compounds from reaction dict, assigning them
-        indices in lexicographical order of SMILES.
-
-        Args
-        ----
-        reactions: dict
-            Reaciton id -> {'smarts': , }
-
-        Returns
-        -------
-        compounds:dict
-            ID, i.e., row / col idx in compound x compound similarity matrix -> smiles and names
-        smi2id:dict
-            Smiles -> cxc similarity matrix
-        '''
-        tmp = {}
-        for elt in reactions.values():
-            smis = chain(*[side.split(".") for side in elt['smarts'].split(">>")])
-            for smi in smis:
-                name = elt['smi2name'][smi]
-                tmp[smi] =  name if name else ''
-
-        compounds = {}
-        smi2id = {}
-        for i, smi in enumerate(sorted(tmp.keys())): # Lexicographical sort of keys will be order of smi2id
-            compounds[i] = {'smiles': smi, 'name': tmp[smi]}
-            smi2id[smi] = i
-        
-        return compounds, smi2id
-
+       
 if __name__ == '__main__':
-    from krxns.config import filepaths
-    from krxns.utils import str2int
     import json
-
-    # Load unpaired coreactants
-    with open(filepaths['coreactants'] / 'unpaired_whitelist.json', 'r') as f:
-        unpaired_whitelist = json.load(f)
-
-    # Load known reaction data
-    with open(filepaths['data'] / 'sprhea_240310_v3_mapped.json', 'r') as f:
-        krs = json.load(f)
-
-    # Load op connected reactions
-    with open(filepaths['connected_reactions'] / 'sprhea_240310_v3_mapped_operator.json', 'r') as f:
-        op_cxns = str2int(json.load(f))
-
-    # Load sim connected reactions
-    with open(filepaths['connected_reactions'] / 'sprhea_240310_v3_mapped_similarity.json', 'r') as f:
-        sim_cxn = str2int(json.load(f))
-
-    with open(filepaths['connected_reactions'] / 'sprhea_240310_v3_mapped_side_counts.json', 'r') as f:
-        side_counts = str2int(json.load(f))
-
-    # Load coreactant whitelist
-    with open(filepaths['coreactants'] / 'pickaxe_whitelist.json', 'r') as f:
-        coreactant_whitelist = json.load(f)
-
-    # Get known compounds
-    kcs, smi2id = extract_compounds(krs)
+    from pathlib import Path
+    root_dir = Path(__file__).parent.parent
+    kcs = pd.read_csv(root_dir / "data/interim/compounds.csv")
+    with open(root_dir / "data/interim/mass_links.json", 'r') as f:
+        mass_inlinks = json.load(f)
 
     edges, nodes = construct_reaction_network(
-        operator_connections=op_cxns,
-        similarity_connections=sim_cxn,
-        side_counts=side_counts,
-        reactions=krs,
-        unpaired_whitelist=unpaired_whitelist,
-        connect_nontrivial=False,
-        atom_lb=0.0,
-        coreactant_whitelist=coreactant_whitelist,
-        add_multi_mol_nodes=True
+        mass_inlinks=mass_inlinks,
+        compounds=kcs,
+        sources=[]
     )
--- scripts/connect_reactions.py
+++ scripts/connect_reactions.py
@@ -5,8 +5,32 @@ from pathlib import Path
 import hydra
 from omegaconf import DictConfig
 from rdkit import Chem
+import json
 
-def mass_balance(am_rxn: str) -> dict[int, dict[int, float]]:
+def compound_id_rxn(rxn: str, smi_to_cpid: dict[str, int]) -> tuple[list[int], list[int]]:
+    '''
+    Converts a reaction string to a tuple of lists of compound ids for reactants and products.
+
+    Args
+    ----
+    rxn:str
+        Reaction string in the form of "R1.R2.R3>>P1.P2.P3"
+
+    smi_to_cpid:dict
+        Dictionary mapping smiles to compound ids.
+
+    Returns
+    -------
+    tuple[list[int], list[int]]
+        Tuple containing two lists: reactant compound ids and product compound ids.
+    '''
+    rcts, pdts = rxn.split('>>')
+    rct_ids = [smi_to_cpid[smi] for smi in rcts.split('.')]
+    pdt_ids = [smi_to_cpid[smi] for smi in pdts.split('.')]
+    
+    return rct_ids, pdt_ids
+
+def mass_balance(am_rxn: str, cpdid_rxn: tuple[list[int], list[int]]) -> dict[int, dict[int, float]]:
     '''
     Returns fraction of atoms in a reactant / product coming from a product / reactant, respectivley.
 
@@ -14,17 +38,19 @@ def mass_balance(am_rxn: str) -> dict[int, dict[int, float]]:
     ----
     am_rxn:str
         Atom-mapped reaction string in the form of "R1.R2.R3>>P1.P2.P3"
-
+    cpdid_rxn:tuple[list[int], list[int]]
+        Tuple containing two lists: reactant compound ids and product compound ids.
+        Must be in same order as atom mapped reaction.
+    
     Returns
     -------
     pdt_inlinks:dict
-        {pdt_idx: {rct_idx: fraction_atoms_from_rct_idx}, }
+        {pdt_id: {rct_id: fraction_atoms_from_rct_id}, }
     '''
     rcts, pdts = [
         [Chem.MolFromSmiles(mol) for mol in side.split('.')]
         for side in am_rxn.split('>>')
     ]
-    n_atoms_pdts = [mol.GetNumAtoms() for mol in pdts]
     
     # Collect atom map numbers to rct / pdt indices
     amn_to_rct_idx = {}
@@ -55,18 +81,18 @@ def mass_balance(am_rxn: str) -> dict[int, dict[int, float]]:
     amns = set(_amns) & set(amns_)
     if len(amns) != len(_amns) or len(amns) != len(amns_):
         raise ValueError("Atom map numbers are not 1-to-1 between reactants and products.")
-
     # Count atoms received by molecule i from molecule j
-    pdt_inlinks = {i: {j: 0.0 for j in range(len(rcts))} for i in range(len(pdts))}
+    pdt_inlinks = {i_id: {j_id: 0.0 for j_id in cpdid_rxn[0]} for i_id in cpdid_rxn[1]}
     for amn in amns:
-        rct_idx = amn_to_rct_idx[amn]
-        pdt_idx = amn_to_pdt_idx[amn]
-        pdt_inlinks[pdt_idx][rct_idx] += 1.0
+        rct_id = cpdid_rxn[0][amn_to_rct_idx[amn]]
+        pdt_id = cpdid_rxn[1][amn_to_pdt_idx[amn]]
+        pdt_inlinks[pdt_id][rct_id] += 1.0
 
     # Normalize by number of atoms in reactants / products
-    for pdt_idx, rct_dict in pdt_inlinks.items():
-        for rct_idx, count in rct_dict.items():
-            pdt_inlinks[pdt_idx][rct_idx] = count / n_atoms_pdts[pdt_idx]
+    for pdt_id, rct_dict in pdt_inlinks.items():
+        tot_atoms = sum(rct_dict.values())
+        for rct_id, count in rct_dict.items():
+            pdt_inlinks[pdt_id][rct_id] = count / tot_atoms
 
     return pdt_inlinks
     
@@ -84,10 +110,47 @@ def main(cfg: DictConfig):
     overlap = rc_0_mapped.rxn_id.isin(mechinformed_mapped.rxn_id)
     mapped_rxns = pd.concat([mechinformed_mapped, rc_0_mapped[~overlap]], ignore_index=True)
 
+    # Get smi2name out of sprhea json
+    with open(Path(cfg.filepaths.raw_data) / cfg.sprhea, 'r') as f:
+        sprhea = json.load(f)
+
+    smi2name = {smi: name for entry in sprhea.values() for smi, name in entry['smi2name'].items()}
+    del sprhea
+
+    # TODO: Standardize (incl tautomer form) all smiles in these reactions, preseriving the atom maping ans so on
+    # TODO: Ultimately, handle cpd smiles canonicalization in pre-processing
+
+    # Extract compounds from reactions
+    compounds = set()
+    for _, row in mapped_rxns.iterrows():
+        for side in row['smarts'].split('>>'):
+            for smi in side.split('.'):
+                compounds.add(smi)
+
+    compounds = sorted(compounds)
+    compounds_df = pd.DataFrame({
+        'id': range(len(compounds)),
+        'smiles': compounds,
+        'name': [smi2name.get(smi, '') for smi in compounds],
+    })
+
+    compounds_df.to_csv(
+        Path(cfg.filepaths.interim_data) / "compounds.csv",
+        index=False
+    )
+
+    # Add compound id reactions to enable canonical compound index in ultimate network
+    smi_to_cpid = dict(zip(compounds_df['smiles'], compounds_df['id']))
+    mapped_rxns["cpdid_rxn"] = mapped_rxns["smarts"].apply(
+        lambda rxn: compound_id_rxn(rxn, smi_to_cpid)
+    )
+
+    # Get mass weighted inlink dicts 
     mass_links = {}
     for _, row in tqdm(mapped_rxns.iterrows(), total=len(mapped_rxns)):
         am_rxn = row['am_smarts']
-        pdt_inlinks = mass_balance(am_rxn)
+        cpdid_rxn = row['cpdid_rxn']
+        pdt_inlinks = mass_balance(am_rxn, cpdid_rxn)
         mass_links[row['rxn_id']] = pdt_inlinks
 
     with open(Path(cfg.filepaths.interim_data) / "mass_links.json", 'w') as f:
